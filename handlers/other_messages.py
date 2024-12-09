8ac3bec4c835de6e9cb8ecacf5681ceded0a42f4
aruuuuuuke <askatovaaruuke657@gmail.com>
2024-12-09 19:36:25 +0600

    hand

--- handlers/other_messages.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router, types
-
-echo_handlr = Router()
-
-@echo_handlr.message()
-async def echo_handler(message: types.Message):
-    txt = message.text
-    await message.answer(txt)
